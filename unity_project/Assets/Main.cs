04769f919dfc3355e49dd3b680e706754293ef43
dmccue <dmccue@localhost>
2015-01-23 19:56:53 -0800

    created a cube!

--- unity_project/Assets/Main.cs
+++ unity_project/Assets/Main.cs
@@ -5,7 +5,8 @@ public class Main : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube.transform.position = new Vector3(0, 0.5F, 0);
 	}
 	
 	// Update is called once per frame
