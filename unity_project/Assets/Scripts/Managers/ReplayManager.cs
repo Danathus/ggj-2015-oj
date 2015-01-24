32f6598813d342450e723be0147ceef795b5f5a3
dmccue <Danathus@users.noreply.github.com>
2015-01-23 23:25:42 -0800

    implemented second iteration of a basic behavior-control scheme pattern

---
+++ unity_project/Assets/Scripts/Managers/ReplayManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReplayManager : Singleton<ReplayManager> {
+  protected ReplayManager () {}
+
+	// Use this for initialization
+	void Start () {
+    Debug.Log("ReplayManager Started");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
