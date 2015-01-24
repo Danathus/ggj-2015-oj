64315cd5d6b828c0e88a572fb583ee78ccd150d3
zfeng <zfeng@localhost>
2015-01-24 14:34:59 -0800

    work in progress on character IK -- looks ugly, but something is working

---
+++ unity_project/Assets/Scripts/Scenarios/boxes.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+// BEHAVIOR ENTRY POINT ////////////////////////////////////////////////////////
+
+public class boxes : MonoBehaviour
+{
+	public GameObject mPlayer1, mPlayer2;
+	private ControlScheme mControls;
+
+	// Use this for initialization
+	void Start () {
+		// create the behaviors in this scene
+		Scenario scenario = new Scenario();
+		float speed = 0.1f;
+		scenario.AddBehavior(new TranslateBehavior("player1 move up",    mPlayer1, new Vector3( 0,  1, 0) * speed));
+		scenario.AddBehavior(new TranslateBehavior("player1 move down",  mPlayer1, new Vector3( 0, -1, 0) * speed));
+		scenario.AddBehavior(new TranslateBehavior("player1 move left",  mPlayer1, new Vector3(-1,  0, 0) * speed));
+		scenario.AddBehavior(new TranslateBehavior("player1 move right", mPlayer1, new Vector3( 1,  0, 0) * speed));
+		scenario.AddBehavior(new TranslateBehavior("player2 move up",    mPlayer2, new Vector3( 0,  1, 0) * speed));
+		scenario.AddBehavior(new TranslateBehavior("player2 move down",  mPlayer2, new Vector3( 0, -1, 0) * speed));
+		scenario.AddBehavior(new TranslateBehavior("player2 move left",  mPlayer2, new Vector3(-1,  0, 0) * speed));
+		scenario.AddBehavior(new TranslateBehavior("player2 move right", mPlayer2, new Vector3( 1,  0, 0) * speed));
+
+		// create the control scheme that maps inputs to these behaviors
+		mControls = new ControlScheme();
+		mControls.AddControl(new KeyCodeControlSignal(KeyCode.W),          scenario.GetBehavior("player1 move up"));
+		mControls.AddControl(new KeyCodeControlSignal(KeyCode.S),          scenario.GetBehavior("player1 move down"));
+		mControls.AddControl(new KeyCodeControlSignal(KeyCode.A),          scenario.GetBehavior("player1 move left"));
+		mControls.AddControl(new KeyCodeControlSignal(KeyCode.D),          scenario.GetBehavior("player1 move right"));
+		mControls.AddControl(new KeyCodeControlSignal(KeyCode.UpArrow),    scenario.GetBehavior("player2 move up"));
+		mControls.AddControl(new KeyCodeControlSignal(KeyCode.DownArrow),  scenario.GetBehavior("player2 move down"));
+		mControls.AddControl(new KeyCodeControlSignal(KeyCode.LeftArrow),  scenario.GetBehavior("player2 move left"));
+		mControls.AddControl(new KeyCodeControlSignal(KeyCode.RightArrow), scenario.GetBehavior("player2 move right"));
+	}
+	
+	// called once per timestep update (critical: do game state updates here!!!)
+	void FixedUpdate()
+	{
+		// update all the controls
+		mControls.Update();
+
+		if (Input.GetKey(KeyCode.Space))
+		{
+			mPlayer1.transform.position = new Vector3(-1, 0, 0);
+			mPlayer2.transform.position = new Vector3( 1, 0, 0);
+			ReplayManager.Instance.Play();
+		}
+	}
+}
+
+// END OF FILE /////////////////////////////////////////////////////////////////
