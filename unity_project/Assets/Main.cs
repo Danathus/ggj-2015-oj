f720d842d9234ac3c7478ed3ff0bb6b8d17a7d41
lochemage <lochemage@gmail.com>
2015-01-23 21:15:19 -0800

    Added folder structure.

---
+++ unity_project/Assets/Main.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour
+{
+	private GameObject mCube;
+
+	// Use this for initialization
+	void Start () {
+		mCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		mCube.transform.position = new Vector3(0, 0.5F, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log("hellooo");
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+            mCube.transform.position = mCube.transform.position - new Vector3(0, 0.1F, 0);
+		}
+	}
+}
