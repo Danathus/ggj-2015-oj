dcfb678639ed68f18dd27bd1709459a1ff354879
lochemage <lochemage@gmail.com>
2015-01-23 22:26:20 -0800

    Added a replay event base class.

---
+++ unity_project/Assets/Scripts/ReplayEvent.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReplayEvent {
+
+  private float _time = 0.0f;
+
+  public float mTime {
+    get { return _time; }
+    private set { _time = value; }
+  }
+
+  ReplayEvent () {
+    mTime = Time.time;
+  }
+
+  public void Activate() {
+
+  }
+}
