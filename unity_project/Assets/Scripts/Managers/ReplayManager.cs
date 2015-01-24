b876cc0e8b3542e1d79249bbc6afbac3d719e498
lochemage <lochemage@gmail.com>
2015-01-23 21:36:59 -0800

    Replay Manager is now a singleton and a prefab.

--- unity_project/Assets/Scripts/Managers/ReplayManager.cs
+++ unity_project/Assets/Scripts/Managers/ReplayManager.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ReplayManager : MonoBehaviour {
+public class ReplayManager : Singleton<ReplayManager> {
+  protected ReplayManager () {}
 
 	// Use this for initialization
 	void Start () {
-	
+    Debug.Log("ReplayManager Started");
 	}
 	
 	// Update is called once per frame
