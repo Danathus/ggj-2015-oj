707b0744ba1e379e7c26f15a00e2c8f39461d386
dmccue <dmccue@localhost>
2015-01-23 21:03:35 -0800

    got very, very basic "player" interaction working

--- unity_project/Assets/Main.cs
+++ unity_project/Assets/Main.cs
@@ -1,16 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Main : MonoBehaviour {
+public class Main : MonoBehaviour
+{
+	private GameObject mCube;
 
 	// Use this for initialization
 	void Start () {
-		GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-		cube.transform.position = new Vector3(0, 0.5F, 0);
+		mCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		mCube.transform.position = new Vector3(0, 0.5F, 0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log("hellooo");
+		//Debug.Log("hellooo");
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+            mCube.transform.position = mCube.transform.position - new Vector3(0, 0.1F, 0);
+		}
 	}
 }
