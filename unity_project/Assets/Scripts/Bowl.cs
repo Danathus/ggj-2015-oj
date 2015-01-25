eb63bc9c27ddff13d8308e9e2814087344ab6926
Jingjing Lu <Jingjing Lu@localhost>
2015-01-24 20:04:09 -0800

    need pull stuff

---
+++ unity_project/Assets/Scripts/Bowl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bowl : MonoBehaviour {
+
+	public float speed = 150.0f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate() {
+		float x = 0, y = 0;
+
+		//this.rigidbody.velocity = new Vector3(0, 1, 0);
+
+		if (Input.GetKey(KeyCode.A)) {
+			x -= speed;
+		}
+		if (Input.GetKey(KeyCode.D)) {
+			x += speed;
+		}
+		if (Input.GetKey(KeyCode.W)) {
+			y += speed;
+		}
+		if (Input.GetKey(KeyCode.S)) {
+			y -= speed;
+		}
+
+		//this.rigidbody.position += new Vector3(x, 0, y);
+		this.rigidbody.velocity = new Vector3(x, 0, y) * Time.fixedDeltaTime;
+		// Debug.Log (Time.fixedDeltaTime);
+	}
+}
---
+++ unity_project/Assets/Scripts/States/VictoryState.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VictoryState: State {
+
+	private Scenario mScenario = null;
+
+	public VictoryState() {
+		mName = "Victory";
+	}
+
+	public override void Enter() {
+		ScenarioManager.Instance.ShowVictory();
+		ReplayManager.Instance.Play();
+
+		GameObject replayCamera = GameObject.FindWithTag("ReplayCamera");
+		if (replayCamera != null) {
+			Camera.main.enabled = false;
+			replayCamera.GetComponent<Camera>().enabled = true;
+		}
+	}
+
+	public override void Leave() {
+		ScenarioManager.Instance.HideVictory();
+		ReplayManager.Instance.Stop();
+	}
+	
+	public override void Update () {
+		if (!ReplayManager.Instance.mIsReplaying) {
+			if (mScenario == null) {
+				mScenario = Object.FindObjectOfType(typeof(Scenario)) as Scenario;
+			}
+
+			if (Input.GetKey(KeyCode.Space)) {
+				if (mScenario != null) {
+					mScenario.Reset();
+					ReplayManager.Instance.Play();
+				}
+			}
+		}
+
+		if (Input.GetKey(KeyCode.Return)) {
+			ScenarioManager.Instance.ActivateState("Play");
+		}
+	}
+}
