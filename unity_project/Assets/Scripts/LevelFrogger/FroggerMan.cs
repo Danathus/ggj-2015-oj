c81392042be90a4430d73bc59bdecb78d7060d60
lochemage <lochemage@gmail.com>
2015-01-25 01:18:21 -0800

    Added missing meta files.

---
+++ unity_project/Assets/Scripts/LevelFrogger/FroggerMan.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class FroggerMan : Scenario {
+
+	public float speed = 4.0f;
+
+	private Animator _animator;
+	private bool _enabled = true;
+	private Vector3 _movement = new Vector3();
+
+	public override void Reset ()
+	{
+	}
+
+	// Use this for initialization
+	void Start () {
+		_animator = GetComponent<Animator>();
+
+		// create behaviors
+		Behavior p1Up    = new MovementCallbackBehavior("player1 move up",    this.gameObject, new Vector3( 0,  0, -1), Move);
+		Behavior p1Down  = new MovementCallbackBehavior("player1 move down",  this.gameObject, new Vector3( 0,  0,  1), Move);
+		Behavior p1Left  = new MovementCallbackBehavior("player1 move left",  this.gameObject, new Vector3( 1,  0,  0), Move);
+		Behavior p1Right = new MovementCallbackBehavior("player1 move right", this.gameObject, new Vector3(-1,  0,  0), Move);
+
+		// rig control scheme
+		//   for buttons
+		mControls.AddControl(new KeyCodeControlSignal(KeyCode.W), p1Up   );
+		mControls.AddControl(new KeyCodeControlSignal(KeyCode.S), p1Down );
+		mControls.AddControl(new KeyCodeControlSignal(KeyCode.A), p1Left );
+		mControls.AddControl(new KeyCodeControlSignal(KeyCode.D), p1Right);
+		//   for gamepads
+		mControls.AddControl(new GamepadAxisControlSignal(GamepadInput.GamePad.Index.One, GamepadInput.GamePad.Axis.LeftStick, GamepadAxisControlSignal.Dimension.Y,  1.0f), p1Up    );
+		mControls.AddControl(new GamepadAxisControlSignal(GamepadInput.GamePad.Index.One, GamepadInput.GamePad.Axis.LeftStick, GamepadAxisControlSignal.Dimension.Y, -1.0f), p1Down  );
+		mControls.AddControl(new GamepadAxisControlSignal(GamepadInput.GamePad.Index.One, GamepadInput.GamePad.Axis.LeftStick, GamepadAxisControlSignal.Dimension.X, -1.0f), p1Left  );
+		mControls.AddControl(new GamepadAxisControlSignal(GamepadInput.GamePad.Index.One, GamepadInput.GamePad.Axis.LeftStick, GamepadAxisControlSignal.Dimension.X,  1.0f), p1Right );
+
+		foreach (var body in this.GetComponentsInChildren<Rigidbody>()) {
+			body.isKinematic = true;
+			body.collider.enabled = false;
+		}
+		this.collider.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		ScenarioUpdate();
+		
+		mControls.Update ();
+
+
+		if (_movement.sqrMagnitude > 0.0) {
+			//this.transform.LookAt(this.transform.position + _movement);
+		}
+
+		//this.animation.Play ("walking");
+
+
+		if(_movement.z < 0) this.transform.position += new Vector3 (0, 0, _movement.z * Time.fixedDeltaTime);
+		_animator.SetFloat ("Horizontal", -_movement.x);
+		_animator.SetFloat ("Vertical", -_movement.z);
+		_animator.SetFloat ("Turn", 0);
+		_animator.SetBool ("Jump", false);
+		_movement.Set (0, 0, 0);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (_enabled && other.name.StartsWith ("Car")) {
+			foreach (var body in this.GetComponentsInChildren<Rigidbody>()) {
+				body.isKinematic = false;
+				body.collider.enabled = true;
+			}	
+			_animator.enabled = false;
+			_enabled = false;
+		}
+	}
+
+	private void Move(GameObject gameObject, Vector3 offset) {
+		if (_enabled) {
+			_movement += offset * speed;
+		}
+	}
+}
---
+++ unity_project/Assets/Scripts/States/PlayState.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayState: State {
+
+	private Scenario mScenario = null;
+
+	public PlayState() {
+		mName = "Play";
+	}
+
+	public override void Enter() {
+		ReplayManager.Instance.Clear();
+		ScenarioManager.Instance.NextScenario();
+	}
+
+	public override void Leave() {
+		if (mScenario != null) {
+			mScenario.Reset();
+			mScenario = null;
+		}
+	}
+	
+	public override void Update () {
+		if (mScenario == null) {
+			mScenario = Object.FindObjectOfType(typeof(Scenario)) as Scenario;
+		}
+
+		if (mScenario != null) {
+			mScenario.mControls.Update();
+		}
+
+		if (Input.GetKey(KeyCode.Return)) {
+			ScenarioManager.Instance.ActivateState("Play");
+		}
+	}
+}
