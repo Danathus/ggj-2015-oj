6d152e9a425a0a559800f93e6afdf748c102d3ac
lochemage <lochemage@gmail.com>
2015-01-25 14:19:40 -0800

    Review state always clears the replay data after it is left.

---
+++ unity_project/Assets/Scripts/Test/TestReplayEvent.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestReplayEvent : ReplayEvent {
+
+  private string mKey;
+
+  private TestReplayEvent(): base() {}
+  public TestReplayEvent(string key): base() {
+    mKey = key;
+  }
+
+  public override void Activate() {
+    Debug.Log(mKey);
+  }
+}
