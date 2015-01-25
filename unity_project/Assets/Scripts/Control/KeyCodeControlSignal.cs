299da029aac9d217635bfac5e6a9671ef996d0e8
dmccue <Danathus@users.noreply.github.com>
2015-01-25 06:04:33 -0800

    title screen: spent perhaps too much time making the title screen a little bit more of a puzzle -- now brains must high-five (or low-five) in order to pass (if using a keyboard)

--- unity_project/Assets/Scripts/Control/KeyCodeControlSignal.cs
+++ unity_project/Assets/Scripts/Control/KeyCodeControlSignal.cs
@@ -4,16 +4,34 @@ using System.Collections;
 public class KeyCodeControlSignal : ControlSignal
 {
 	KeyCode mKeyCode;
+	KeyCode[] mComplements;
 
-	public KeyCodeControlSignal(KeyCode keyCode)
+	public KeyCodeControlSignal(KeyCode keyCode, KeyCode[] complements = null)
 		: base()
 	{
 		mKeyCode = keyCode;
+		mComplements = complements;
 	}
 
 	public override float PollSignal()
 	{
 		bool keyDown = Input.GetKey(mKeyCode);
-		return keyDown ? 1.0f : 0.0f;
+		float signal = keyDown ? 1.0f : 0.0f;
+		if (mComplements != null)
+		{
+			bool weaken = false;
+			foreach (var complement in mComplements)
+			{
+				if (Input.GetKey(complement))
+				{
+					weaken = !weaken;
+				}
+			}
+			if (weaken)
+			{
+				signal /= Mathf.Sqrt(2.0f);
+			}
+		}
+		return signal;
 	}
 }
