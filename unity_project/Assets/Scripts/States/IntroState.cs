d61a25cfa90fb94f9b2fe6dd69ab02c36d532859
Jesus <Jesus@localhost>
2015-01-25 03:13:45 -0800

    Man Diffuse Texture is added in plus the material on Man is updated

---
+++ unity_project/Assets/Scripts/States/IntroState.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroState: State {
+
+	public IntroState() {
+		mName = "Intro";
+	}
+
+	public override void Enter() {
+		ScenarioManager.Instance.Shuffle();
+		if (!ScenarioManager.Instance.isInitialized()) {
+			Application.LoadLevel("main_menu");
+		} else {
+			ScenarioManager.Instance.ActivateState("Play");
+		}
+	}
+
+	public override void Leave() {
+
+	}
+	
+	public override void Update () {
+		if (Input.GetKey(KeyCode.Return)) {
+			ScenarioManager.Instance.ActivateState("Play");
+		}
+	}
+}
