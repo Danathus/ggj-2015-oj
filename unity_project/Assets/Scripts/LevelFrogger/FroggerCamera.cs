8430c186174bec1796389c2bf4c9af73d2b909ad
lochemage <lochemage@gmail.com>
2015-01-25 11:56:17 -0800

    Added wind conditions to the cereal pour scenario.

---
+++ unity_project/Assets/Scripts/LevelFrogger/FroggerCamera.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using System.Collections;
+
+public class FroggerCamera : Scenario {
+
+	public Transform cameraTarget;
+	public float targetDist = 4;
+	public float targetY = 2.5f;
+	public float speed = 12;
+
+	public override void Reset()
+	{
+	}
+
+	// Use this for initialization
+	void Start() {
+		Move(this.gameObject, new Vector3 ());
+
+		// create behaviors
+		Behavior p2Left  = new MovementCallbackBehavior("player2 move left",  this.gameObject, new Vector3( 1,  0,  0) * Time.fixedDeltaTime, Move);
+		Behavior p2Right = new MovementCallbackBehavior("player2 move right", this.gameObject, new Vector3(-1,  0,  0) * Time.fixedDeltaTime, Move);
+
+		// rig control scheme
+		//   for buttons
+		mControls.AddControl(new KeyCodeControlSignal(KeyCode.LeftArrow),  p2Left );
+		mControls.AddControl(new KeyCodeControlSignal(KeyCode.RightArrow), p2Right);
+		//   for gamepads
+		mControls.AddControl(new GamepadAxisControlSignal(GamepadInput.GamePad.Index.Two, GamepadInput.GamePad.Axis.LeftStick, GamepadAxisControlSignal.Dimension.X, -1.0f), p2Left  );
+		mControls.AddControl(new GamepadAxisControlSignal(GamepadInput.GamePad.Index.Two, GamepadInput.GamePad.Axis.LeftStick, GamepadAxisControlSignal.Dimension.X,  1.0f), p2Right );
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate() {
+		//Debug.Log (Input.GetKey (KeyCode.LeftArrow));
+		this.transform.LookAt(cameraTarget);
+
+		Vector3 position = this.transform.position;
+		Vector3 targetPos = cameraTarget.position;
+		Vector3 dir = position - targetPos;
+		dir.y = 0;
+		position = targetPos + dir.normalized * targetDist;
+		position.y = targetY;
+		this.transform.position = position;
+
+		mControls.Update();
+		//ScenarioUpdate();
+	}
+
+	private void Move(GameObject gameObject, Vector3 offset) {
+		Vector3 leftDir = gameObject.transform.TransformDirection(new Vector3(-1, 0, 0));
+		gameObject.transform.position += leftDir * offset.x * speed;
+	}
+}
---
+++ unity_project/Assets/Scripts/States/State.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class State {
+
+	public string mName;
+
+	public State() {}
+	public abstract void Enter();
+	public abstract void Leave();
+	public abstract void Update();
+
+	protected bool ShouldAdvanceState()
+	{
+		return Input.GetKey(KeyCode.Return);
+	}
+}
