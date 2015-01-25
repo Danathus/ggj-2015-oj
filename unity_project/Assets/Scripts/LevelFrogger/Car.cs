3df0b7e867b29f64b9d39a87d4a65139ba2dd9ef
dmccue <Danathus@users.noreply.github.com>
2015-01-25 03:00:00 -0800

    elevator: fixed a problem that was causing the man to not respond to user input. The problem was we had two UnstableHands in the scene -- one for the man and one for the gun! I toggled the gun off, and that solved the problem. Took a while to find first though. :P

---
+++ unity_project/Assets/Scripts/LevelFrogger/Car.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Car : MonoBehaviour {
+
+	public float speed = 50.0f;
+	public float lifeTime = 10;
+
+	private float _timeAlive = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		_timeAlive += Time.fixedDeltaTime;
+		if (_timeAlive > lifeTime) {
+			Destroy(this.gameObject);
+			return;
+		}
+
+		this.transform.position += this.transform.TransformDirection(new Vector3(0, 0, 1)) * speed * Time.fixedDeltaTime;
+	}
+}
