d9cf53a2c298e04f26c7b75c405f5146205f045e
German <German@localhost>
2015-01-24 20:53:17 -0800

    Cherrios and unicycle added

---
+++ unity_project/Assets/Scripts/States/IntroState.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroState: State {
+
+	public IntroState() {
+		mName = "Intro";
+	}
+
+	public override void Enter() {
+		ScenarioManager.Instance.Shuffle();
+		Application.LoadLevel("main_menu");
+	}
+
+	public override void Leave() {
+
+	}
+	
+	public override void Update () {
+		if (Input.GetKey(KeyCode.Return)) {
+			ScenarioManager.Instance.ActivateState("Play");
+		}
+	}
+}
