1bb3e649ca3318f648d225e82afdd74f6d52626e
dmccue <Danathus@users.noreply.github.com>
2015-01-25 03:41:21 -0800

    title screen: now both players must work together to start the game, and they must hold a pose for a moment. Brought magnet effect in tighter, moved them further apart to start. Might require sticks, heh. Analog > digital.

---
+++ unity_project/Assets/Scripts/Bowl.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bowl : Scenario {
+
+	public float speed = 150.0f;
+
+	private int _totalCaught = 0;
+	private float _playTime = 0.0f;
+	private bool _hasEnded = false;
+
+	public override void Reset ()
+	{
+	}
+
+	public void CaughtCereal()
+	{
+		_totalCaught ++;
+	}
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate() {
+		float x = 0, y = 0;
+
+		//this.rigidbody.velocity = new Vector3(0, 1, 0);
+
+		if (Input.GetKey(KeyCode.A)) {
+			x -= speed;
+		}
+		if (Input.GetKey(KeyCode.D)) {
+			x += speed;
+		}
+		if (Input.GetKey(KeyCode.W)) {
+			y += speed;
+		}
+		if (Input.GetKey(KeyCode.S)) {
+			y -= speed;
+		}
+
+		if (!_hasEnded) {
+			_playTime += Time.fixedDeltaTime;
+			if(_totalCaught == 100)
+			{
+				this.Victory();
+				_hasEnded = true;
+				Debug.Log ("Victory");
+			}
+			else if (_playTime > 15) {
+				this.Failure();
+				_hasEnded = true;
+				Debug.Log ("Failure");
+			}
+		}
+
+		//this.rigidbody.position += new Vector3(x, 0, y);
+		this.rigidbody.velocity = new Vector3 (x, 0, y) * -Time.fixedDeltaTime;
+		// Debug.Log (Time.fixeadDeltaTime);
+		//Debug.Log (this.rigidbody.velocity);
+	}
+}
---
+++ unity_project/Assets/Scripts/Managers/ReplayManager.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ReplayManager : Singleton<ReplayManager> {
+  protected ReplayManager () {}
+
+  private List<ReplayEvent> mEventList = new List<ReplayEvent>();
+  private float mCurrentTime = 0.0f;
+  private int mCurrentEvent = -1;
+
+  private bool _isReplaying = false;
+
+
+
+  public bool mIsReplaying {
+    get { return _isReplaying; }
+    private set { _isReplaying = value; }
+  }
+
+  // Use this for initialization
+  void Start () {
+    DontDestroyOnLoad(gameObject);
+  }
+  
+  // Update is called once per frame
+  void FixedUpdate () {
+    if (mIsReplaying) {
+      UpdateReplay();
+    }
+  }
+
+  public void AddEvent(ReplayEvent e) {
+    mEventList.Add(e);
+  }
+
+  public void Clear() {
+    mEventList.Clear();
+  }
+
+  public void Play() {
+    if (mEventList.Count > 0) {
+      mIsReplaying = true;
+      mCurrentEvent = 0;
+      mCurrentTime = mEventList[0].mTime;
+    }
+  }
+
+  public void Stop() {
+    mIsReplaying = false;
+    mCurrentEvent = -1;
+  }
+
+  private void UpdateReplay() {
+    mCurrentTime += Time.fixedDeltaTime;
+
+    if (mCurrentEvent > -1 && mCurrentEvent < mEventList.Count) {
+      while (mCurrentEvent < mEventList.Count && mEventList[mCurrentEvent].mTime <= mCurrentTime) {
+		  Debug.Log("UpdateReplay");
+        mEventList[mCurrentEvent].Activate();
+        mCurrentEvent++;
+      }
+    } else {
+      // Stop the replay automatically when we come to the end of the events.
+      Stop();
+    }
+  }
+}
