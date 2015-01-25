ba5aa796aa6113e9244ac47e488bf861812ed9e1
lochemage <lochemage@gmail.com>
2015-01-25 12:29:04 -0800

    Player controls now swap sides after each full scenario cycle.

--- unity_project/Assets/Scripts/Scenarios/Scenario.cs
+++ unity_project/Assets/Scripts/Scenarios/Scenario.cs
@@ -29,25 +29,25 @@ public abstract class Scenario: MonoBehaviour {
 
 	public void SetControlScheme(int player, Behavior up, Behavior down, Behavior left, Behavior right) {
 		if (ScenarioManager.Instance.m_PrimaryPlayer == player) {
-			mControls.AddControl(new KeyCodeControlSignal(KeyCode.W), up   );
-			mControls.AddControl(new KeyCodeControlSignal(KeyCode.S), down );
-			mControls.AddControl(new KeyCodeControlSignal(KeyCode.A), left );
-			mControls.AddControl(new KeyCodeControlSignal(KeyCode.D), right);
+			if (up != null) mControls.AddControl(new KeyCodeControlSignal(KeyCode.W), up   );
+			if (down != null) mControls.AddControl(new KeyCodeControlSignal(KeyCode.S), down );
+			if (left != null) mControls.AddControl(new KeyCodeControlSignal(KeyCode.A), left );
+			if (right != null) mControls.AddControl(new KeyCodeControlSignal(KeyCode.D), right);
 			//   for gamepads
-			mControls.AddControl(new GamepadAxisControlSignal(GamepadInput.GamePad.Index.One, GamepadInput.GamePad.Axis.LeftStick, GamepadAxisControlSignal.Dimension.Y,  1.0f), up    );
-			mControls.AddControl(new GamepadAxisControlSignal(GamepadInput.GamePad.Index.One, GamepadInput.GamePad.Axis.LeftStick, GamepadAxisControlSignal.Dimension.Y, -1.0f), down  );
-			mControls.AddControl(new GamepadAxisControlSignal(GamepadInput.GamePad.Index.One, GamepadInput.GamePad.Axis.LeftStick, GamepadAxisControlSignal.Dimension.X, -1.0f), left  );
-			mControls.AddControl(new GamepadAxisControlSignal(GamepadInput.GamePad.Index.One, GamepadInput.GamePad.Axis.LeftStick, GamepadAxisControlSignal.Dimension.X,  1.0f), right );
+			if (up != null) mControls.AddControl(new GamepadAxisControlSignal(GamepadInput.GamePad.Index.One, GamepadInput.GamePad.Axis.LeftStick, GamepadAxisControlSignal.Dimension.Y,  1.0f), up    );
+			if (down != null) mControls.AddControl(new GamepadAxisControlSignal(GamepadInput.GamePad.Index.One, GamepadInput.GamePad.Axis.LeftStick, GamepadAxisControlSignal.Dimension.Y, -1.0f), down  );
+			if (left != null) mControls.AddControl(new GamepadAxisControlSignal(GamepadInput.GamePad.Index.One, GamepadInput.GamePad.Axis.LeftStick, GamepadAxisControlSignal.Dimension.X, -1.0f), left  );
+			if (right != null) mControls.AddControl(new GamepadAxisControlSignal(GamepadInput.GamePad.Index.One, GamepadInput.GamePad.Axis.LeftStick, GamepadAxisControlSignal.Dimension.X,  1.0f), right );
 		} else {
-			mControls.AddControl(new KeyCodeControlSignal(KeyCode.UpArrow),    up   );
-			mControls.AddControl(new KeyCodeControlSignal(KeyCode.DownArrow),  down );
-			mControls.AddControl(new KeyCodeControlSignal(KeyCode.LeftArrow),  left );
-			mControls.AddControl(new KeyCodeControlSignal(KeyCode.RightArrow), right);
+			if (up != null) mControls.AddControl(new KeyCodeControlSignal(KeyCode.UpArrow),    up   );
+			if (down != null) mControls.AddControl(new KeyCodeControlSignal(KeyCode.DownArrow),  down );
+			if (left != null) mControls.AddControl(new KeyCodeControlSignal(KeyCode.LeftArrow),  left );
+			if (right != null) mControls.AddControl(new KeyCodeControlSignal(KeyCode.RightArrow), right);
 			//   for gamepads
-			mControls.AddControl(new GamepadAxisControlSignal(GamepadInput.GamePad.Index.Two, GamepadInput.GamePad.Axis.LeftStick, GamepadAxisControlSignal.Dimension.Y,  1.0f), up    );
-			mControls.AddControl(new GamepadAxisControlSignal(GamepadInput.GamePad.Index.Two, GamepadInput.GamePad.Axis.LeftStick, GamepadAxisControlSignal.Dimension.Y, -1.0f), down  );
-			mControls.AddControl(new GamepadAxisControlSignal(GamepadInput.GamePad.Index.Two, GamepadInput.GamePad.Axis.LeftStick, GamepadAxisControlSignal.Dimension.X, -1.0f), left  );
-			mControls.AddControl(new GamepadAxisControlSignal(GamepadInput.GamePad.Index.Two, GamepadInput.GamePad.Axis.LeftStick, GamepadAxisControlSignal.Dimension.X,  1.0f), right );
+			if (up != null) mControls.AddControl(new GamepadAxisControlSignal(GamepadInput.GamePad.Index.Two, GamepadInput.GamePad.Axis.LeftStick, GamepadAxisControlSignal.Dimension.Y,  1.0f), up    );
+			if (down != null) mControls.AddControl(new GamepadAxisControlSignal(GamepadInput.GamePad.Index.Two, GamepadInput.GamePad.Axis.LeftStick, GamepadAxisControlSignal.Dimension.Y, -1.0f), down  );
+			if (left != null) mControls.AddControl(new GamepadAxisControlSignal(GamepadInput.GamePad.Index.Two, GamepadInput.GamePad.Axis.LeftStick, GamepadAxisControlSignal.Dimension.X, -1.0f), left  );
+			if (right != null) mControls.AddControl(new GamepadAxisControlSignal(GamepadInput.GamePad.Index.Two, GamepadInput.GamePad.Axis.LeftStick, GamepadAxisControlSignal.Dimension.X,  1.0f), right );
 		}
 	}
 
