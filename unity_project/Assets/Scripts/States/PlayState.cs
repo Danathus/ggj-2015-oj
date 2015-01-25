87fe589d92477e871fa2c90619a1c1d0714b322d
dmccue <Danathus@users.noreply.github.com>
2015-01-25 04:37:28 -0800

    fixes for review state -- now there is a 3 second countdown upon victory/failure, before starting the replay. Also fixed a bug that was causing immediate reset on leaving the play state (we want to linger for a moment at the start of the review state)

---
+++ unity_project/Assets/Scripts/States/PlayState.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayState: State {
+
+	private Scenario[] mScenarioList = null;
+
+	public PlayState() {
+		mName = "Play";
+	}
+
+	public override void Enter() {
+		ReplayManager.Instance.Clear();
+		ScenarioManager.Instance.NextScenario();
+	}
+
+	public override void Leave() {
+		if (mScenarioList != null) {
+			mScenarioList = null;
+		}
+	}
+	
+	public override void Update () {
+		if (mScenarioList == null) {
+			mScenarioList = Object.FindObjectsOfType<Scenario>();
+		}
+
+		foreach (var scenario in mScenarioList) {
+			scenario.mControls.Update();
+		}
+
+		if (Input.GetKey(KeyCode.Return)) {
+			ScenarioManager.Instance.ActivateState("Play");
+		}
+	}
+}
