37ef7de46664221859a699da38cb46a47b2d3020
lochemage <lochemage@gmail.com>
2015-01-24 23:53:51 -0800

    Added title screen to the main menu.

---
+++ unity_project/Assets/Scripts/Scenarios/TitleScreen.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScreen : MonoBehaviour {
+
+	public GameObject m_LeftPart = null;
+	public GameObject m_RightPart = null;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (m_LeftPart != null && m_RightPart != null) {
+			
+		}
+	}
+}
