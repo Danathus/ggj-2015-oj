9118a751856c645bcd753d2720d12ca9c917f4ab
lochemage <lochemage@gmail.com>
2015-01-25 13:03:04 -0800

    Player info text now swaps player numbers when necessary.  Also fixed info text and time limit for Unicycle scenario.

---
+++ unity_project/Assets/Scripts/States/State.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class State {
+
+	public string mName;
+
+	public State() {}
+	public abstract void Enter();
+	public abstract void Leave();
+	public abstract void Update();
+
+	protected bool ShouldAdvanceState()
+	{
+		return Input.GetKey(KeyCode.Return) ||
+			GamepadInput.GamePad.GetButton(
+				GamepadInput.GamePad.Button.Start,
+				GamepadInput.GamePad.Index.One) ||
+			GamepadInput.GamePad.GetButton(
+				GamepadInput.GamePad.Button.Start,
+				GamepadInput.GamePad.Index.Two);
+	}
+}
