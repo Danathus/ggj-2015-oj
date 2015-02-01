4c5dc7bfb77ab59b06c56bf39802fec8267672d8
cmurphy <cmurphy@localhost>
2015-01-31 21:51:58 -0800

    fixed difficulty levels on cereal pour level

---
+++ unity_project/Assets/Scripts/States/ReviewState.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using System.Collections;
+
+public class ReviewState: State {
+	float mCountdownToReplay;
+
+	public ReviewState() {
+		mName = "Review";
+	}
+
+	public override void Enter() {
+		mCountdownToReplay = 3.0f;
+
+		GameObject replayCamera = GameObject.FindWithTag("ReplayCamera");
+		if (replayCamera != null) {
+			Camera.main.enabled = false;
+			replayCamera.GetComponent<Camera>().enabled = true;
+		}
+	}
+
+	public override void Leave() {
+		ReplayManager.Instance.Stop();
+		ReplayManager.Instance.Clear();
+		ScenarioManager.Instance.HideVinette();
+	}
+
+	// called at FixedUpdate()
+	public override void Update () {
+		if (!ReplayManager.Instance.mIsReplaying) {
+			// If the countdown is already elapsed and we get here,
+			// it is because the replay has just finished playing.
+			if (mCountdownToReplay <= 0) {
+				ScenarioManager.Instance.NextScenario();
+				return;
+			}
+
+			mCountdownToReplay -= Time.fixedDeltaTime;
+			if (mCountdownToReplay <= 0.0f) {
+				bool updatedSomeScenario = false;
+				Scenario[] scenarioList = Object.FindObjectsOfType<Scenario>();
+				foreach (var scenario in scenarioList) {
+					scenario.Reset();
+					updatedSomeScenario = true;
+				}
+				if (updatedSomeScenario) {
+					Debug.Log("Replay!");
+					ReplayManager.Instance.AddEvent(new BookendEvent()); // add a bookend first to mark end
+					ReplayManager.Instance.Play();
+				}
+				ScenarioManager.Instance.ShowVinette();
+			}
+		}
+
+		if (base.ShouldAdvanceState()) {
+			ScenarioManager.Instance.NextScenario();
+		}
+	}
+}
