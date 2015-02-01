e6aa5438a3d7917bb05d264113f1fa9f0e6cbc96
lochemage <lochemage@gmail.com>
2015-01-31 23:37:20 -0800

    Added an indicator arrow to show you which elevator button to press that only shows on easy difficulty.

---
+++ unity_project/Assets/Scripts/LevelUni/uiwheelrotate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class uiwheelrotate : MonoBehaviour {
+	public float speed = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		this.transform.Rotate(new Vector3 (0, 0, speed * Time.fixedDeltaTime));
+	}
+}
---
+++ unity_project/Assets/Scripts/Scenarios/ElevatorArrow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElevatorArrow : MonoBehaviour {
+
+	public GameObject m_Floor2Arrow = null;
+	public GameObject m_Floor1Arrow = null;
+	public GameObject m_FloorGArrow = null;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
